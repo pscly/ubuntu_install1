1d1337034817097648bc2b19ea62ad305aea0dc7
pscly <pscly@outlook.com>
2020-11-17 11:56:08 +0800

    就只是修改了一下 换源哪里(加了个备份)

--- gongneng/func_1.py
+++ gongneng/func_1.py
@@ -1,6 +1,9 @@
 import os
 def huanyuan():
+
+    os.system('sudo cp /etc/apt/sources.list /etc/apt/sources.bak.list')
     os.system('sudo cp ./lists/sources.list /etc/apt/sources.list')
+    
     # os.system('sudo apt-get update')
 
     # os.system('sudo apt install python3-pip')
