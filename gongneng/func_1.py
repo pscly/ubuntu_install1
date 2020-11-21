fc6fbfe2c4f36ef821f26c265c32a1c35f5500b2
pscly <pscly@outlook.com>
2020-11-21 13:17:27 +0800

    备份多了年月日-秒

--- gongneng/func_1.py
+++ gongneng/func_1.py
@@ -1,5 +1,13 @@
 import os
+import time
+
 def huanyuan():
+    now_time = time.strftime("%y-%m-%d--%S")  # 时间是 年-月-日--秒
+    os.system(f'sudo cp /etc/apt/sources.list /etc/apt/sources.{now_time}.list')
     os.system('sudo cp ./lists/aliyun.list /etc/apt/sources.list')
     os.system('sudo apt-get update')
     # os.system('sudo apt install python3-pip')
+
+if __name__ == "__main__":
+    print()
+
